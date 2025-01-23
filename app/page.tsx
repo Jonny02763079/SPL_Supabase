3d0a5f86b7c1f85e392b157c3ba01223aaff5a43
Ronny <kostmann.b@gmx.at>
2025-01-23 10:23:49 +0100

    changes type at event

--- app/page.tsx
+++ app/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
     fetchHomeworks();
   }, [selectedSubject]);
 
-  const handleChange = (event: unknown) => {
+  const handleChange = (event: any) => {
     setSelectedSubject(event.target.value);
   };
 
